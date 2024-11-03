0ceed7caee7b755f76783d4199bdd0fb5e2d91d2
Seife1 <seifegebreal.mosisa@aait.edu.et>
2024-11-04 01:33:02 +0300

    minor change

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -8,8 +8,8 @@ def load_data(filepath):
     df.set_index('Date', inplace=True)
     return df
 
-def merge_economic_data(oil_data, economic_data):
+def merge_economic_data(brentoil_data, economic_data):
     """Merge oil price data with economic indicators for multivariate analysis."""
     economic_data = economic_data.reset_index().pivot(index='date', columns='country')
-    merged_data = oil_data.join(economic_data, on='Date', how='left')
+    merged_data = brentoil_data.join(economic_data, on='Date', how='left')
     return merged_data

93e738720c05d0d1598a4beccec3fb6b33e564ae
Seife1 <seifegebreal.mosisa@aait.edu.et>
2024-11-04 10:43:02 +0000

    task 2 starter

--- src/economic_data.py
+++ src/economic_data.py
@@ -3,5 +3,5 @@ import datetime
 
 def fetch_economic_data(indicators, countries, start_date, end_date):
     """Fetch economic data from World Bank for specified indicators and countries."""
-    economic_data = wbdata.get_dataframe(indicators, country=countries, data_date=(start_date, end_date))
+    economic_data = wbdata.get_dataframe(indicators, country=countries, date=(start_date, end_date))
     return economic_data
